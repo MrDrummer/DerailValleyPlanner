a517c8d9d538234c8c0d97cf8da48170476560f5
MrDrummer25 <mrdrummer.programming@hotmail.com>
2023-05-10 15:41:33 +0100

    Added gitignore & various other changes

--- DerailValleyPlanner/MauiProgram.cs
+++ DerailValleyPlanner/MauiProgram.cs
@@ -35,6 +35,12 @@ public static class MauiProgram
 
 #if DEBUG
         builder.Services.AddBlazorWebViewDeveloperTools();
+
+        builder.Services.AddLogging(logging =>
+        {
+            logging.AddDebug();
+        });
+
         builder.Logging.AddDebug();
 #endif
 
