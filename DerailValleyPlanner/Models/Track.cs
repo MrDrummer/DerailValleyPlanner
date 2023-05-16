1204f516c80ce88b80af7147d4d721a2135087d8
MrDrummer <mrdrummer.programming@hotmail.com>
2023-05-16 21:17:22 +0100

    Fixed bug where tertiary statement was backwards

--- DerailValleyPlanner/Models/Track.cs
+++ DerailValleyPlanner/Models/Track.cs
@@ -5,7 +5,7 @@ namespace DerailValleyPlanner.Models;
 public class Track
 {
     public sbyte Id { get; set; }
-    public List<string> Direction { get; set; }
+    public List<string> Directions { get; set; }
     private Yard Yard { get; set; }
     public string Note { get; set; }
     public string TypeName { get; set; }
@@ -14,12 +14,13 @@ public class Track
 
     public Track(Yard yard, YardGroupTrackConfig trackConfig)
     {
+        Console.WriteLine(trackConfig.Direction == null);
         Id = trackConfig.Id;
         Note = trackConfig.Note;
         Yard = yard;
-        Direction = trackConfig.Direction == null
-            ? new List<string> { trackConfig.Direction }
-            : yard.Directions;
+        Directions = trackConfig.Direction == null
+            ? yard.Directions
+            : new List<string> { trackConfig.Direction };
         var des = yard.DesignatorConfig.Find(d => d.Code == trackConfig.Type);
         TypeName = des.Name;
         TypeCode = des.Code;
