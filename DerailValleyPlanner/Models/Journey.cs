74e8a5f4420fb840b0f3122ac1407b4ea0ae8a74
MrDrummer25 <mrdrummer.programming@hotmail.com>
2023-05-25 16:08:14 +0100

    Update stop class

--- DerailValleyPlanner/Models/Journey.cs
+++ DerailValleyPlanner/Models/Journey.cs
@@ -12,27 +12,31 @@ public class Journey
  */
 public class JourneyStop
 {
+    public enum Kind
+    {
+        Load,
+        Unload,
+        Via
+    }
+    
+    [Key]
+    public int StopId { set; get; }
+    
+    [Required]
+    public int Index { get; set; }
+    
     [Required]
     public Yard Yard { get; set; }
     
     [Required]
-    public Type StopType { get; set; }
+    public Kind StopType { get; set; }
     
     [MinLength(0)]
     [MaxLength(1000)]
     public string Note { get; set; }
     
-    
-    
     // public int TotalMass { get; set; }
     // public int TotalLength { get; set; }
     // public int TotalWagons { get; set; }
     // public int TotalPay { get; set; }
-}
-
-public enum StopType
-{
-    Load,
-    Unload,
-    Via
 }
