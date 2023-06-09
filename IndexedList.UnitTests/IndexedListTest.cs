10bc7a50bb58f77265cc441a323abc8e8cf9c382
MrDrummer25 <mrdrummer.programming@hotmail.com>
2023-06-09 11:04:40 +0100

    Added tests for the number of callbacks invoked

--- IndexedList.UnitTests/IndexedListTest.cs
+++ IndexedList.UnitTests/IndexedListTest.cs
@@ -152,7 +152,67 @@ public class IndexedListTest
     }
 
     [Test]
-    public void MoveForwardUpdateIndex()
+    public void MoveForwardsUpdateIndex()
+    {
+        var toEqual = new IndexedList<IndexedInstance>
+        {
+            new()
+            {
+                Index = 0,
+                Key = 0
+            },
+            new()
+            {
+                Index = 1,
+                Key = 1
+            },
+            new()
+            {
+                Index = 2,
+                Key = 3
+            },
+            new()
+            {
+                Index = 3,
+                Key = 2
+            },
+        };
+        
+        _indexedList.Move(3, 2);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(_indexedList[0].Index, Is.EqualTo(0));
+            Assert.That(_indexedList[1].Index, Is.EqualTo(1));
+            Assert.That(_indexedList[2].Index, Is.EqualTo(3));
+            Assert.That(_indexedList[3].Index, Is.EqualTo(2));
+        });
+
+        var updates = 0;
+        
+        _indexedList.ActionOnUpdatedIndex(i => updates++);
+        
+        Assert.That(updates, Is.EqualTo(2));
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(_indexedList[0].Index, Is.EqualTo(toEqual[0].Index));
+            Assert.That(_indexedList[1].Index, Is.EqualTo(toEqual[1].Index));
+            Assert.That(_indexedList[2].Index, Is.EqualTo(toEqual[2].Index));
+            Assert.That(_indexedList[3].Index, Is.EqualTo(toEqual[3].Index));
+        });
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(_indexedList[0].Key, Is.EqualTo(toEqual[0].Key));
+            Assert.That(_indexedList[1].Key, Is.EqualTo(toEqual[1].Key));
+            Assert.That(_indexedList[2].Key, Is.EqualTo(toEqual[2].Key));
+            Assert.That(_indexedList[3].Key, Is.EqualTo(toEqual[3].Key));
+        });
+    }
+
+    [Test]
+    public void MoveBackwardsUpdateIndex()
     {
         var toEqual = new IndexedList<IndexedInstance>
         {
@@ -187,8 +247,12 @@ public class IndexedListTest
             Assert.That(_indexedList[2].Index, Is.EqualTo(3));
             Assert.That(_indexedList[3].Index, Is.EqualTo(0));
         });
+
+        var updates = 0;
         
-        _indexedList.ActionOnUpdatedIndex(i => {});
+        _indexedList.ActionOnUpdatedIndex(i => updates++);
+        
+        Assert.That(updates, Is.EqualTo(4));
         
         Assert.Multiple(() =>
         {
