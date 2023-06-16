f4035ebc9c354b1a968aaa44a31fd91e22947402
MrDrummer25 <mrdrummer.programming@hotmail.com>
2023-06-16 09:42:59 +0100

    ListTest now listens to both the collection and individual items

--- ListTest/Program.cs
+++ ListTest/Program.cs
@@ -1,23 +1,43 @@
 ﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 var list = new ObservableCollection<Indexed>();
+list.CollectionChanged += (sender, args) =>
+{
+    if (args.Action != NotifyCollectionChangedAction.Move) return;
+    Console.WriteLine($"Item at index {args.OldStartingIndex} moved to index {args.NewStartingIndex}");
+
+    var (oldI, newI) = (args.OldStartingIndex, args.NewStartingIndex);
+
+    var start = newI > oldI ? oldI : newI;
+
+    if (sender is not ObservableCollection<Indexed> newItems || newItems.Count == 0) return;
+    for (var i = start; i < newItems.Count; i++)
+    {
+        var item = newItems[i];
+
+        if (item != null && item.Index == i) return;
+        
+        
+        item.Index = i;
+    }
+};
+
 for (var i = 0; i < 10; i++)
 {
-    var item = new Indexed { Index = i };
+    var item = new Indexed { Index = i, Original = i };
     item.PropertyChanging += ItemOnPropertyChanging;
     list.Add(item);
 }
 
-list.Move(0, 1);
-
-list[0].Index = 1;
-list[1].Index = 0;
+list.Move(0, 3);
+list.Move(9, 3);
 
 Thread.Sleep(2000);
 
 static void ItemOnPropertyChanging(object? sender, ValueChanged e)
 {
-    Console.WriteLine($"{e.OldValue} changed to ${e.NewValue}");
+    Console.WriteLine($"{e.OldValue} changed to {e.NewValue}");
 }
 
 public struct ValueChanged
@@ -35,7 +55,9 @@ public class Indexed
     public delegate void ValueChangedEventHandler(object? sender, ValueChanged e);
 
     public event ValueChangedEventHandler? PropertyChanging;
-
+    
+    public int Original { get; set; }
+    
     public int Index
     {
         get => _index;
